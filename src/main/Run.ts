9fe1fb350b4fe5ed7542f2a4e47dfc7e9bb6777d
sym <simoneg@apache.org>
2016-09-12 19:07:10 +0200

    Support for some parameters being optional

--- src/main/Run.ts
+++ src/main/Run.ts
@@ -42,10 +42,14 @@ for (var i = 2; i < process.argv.length; i++) {
 
 function makeUrl(user? :string, pass? :string) {
     var url = 'mongodb://';
-    url += (user || args['u']);
-    url += ':';
-    url += (pass || args['p']);
-    url += '@';
+    if (user || args['u']) {
+        url += (user || args['u']);
+        if (pass || args['p']) {
+            url += ':';
+            url += (pass || args['p']);
+        }
+        url += '@';
+    }
     if (typeof(args['h']) == 'array') {
         for (var i = 0; i < args['h'].length; i++) {
             url += args['h'][i] + ',';
@@ -59,11 +63,11 @@ function makeUrl(user? :string, pass? :string) {
 }
 
 if (!args['curl']) {
-    args['curl'] = makeUrl() + args['d'] + "?replicaSet=" + args['r'];
+    args['curl'] = makeUrl() + args['d'] + (args['r'] ? "?replicaSet=" + args['r'] : '');
 }
 
 if (!args['ourl']) {
-    args['ourl'] = makeUrl(args['ou'], args['op']) + "local?replicaSet=" + args['r'] + "&authSource=admin";
+    args['ourl'] = makeUrl(args['ou'], args['op']) + "local?" + (args['r'] ? "replicaSet=" + args['r'] : '') + "&authSource=admin";
 }
 
 var io = SocketIO();
